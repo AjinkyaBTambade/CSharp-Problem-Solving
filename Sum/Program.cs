13f8bdbd77746fccf48d55abb4c807b5a683bb04
Ajinkya <ajinkyatambade143@gmail.com>
2024-03-20 19:34:49 +0530

    1St C# Code

---
+++ Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+    
+        Console.WriteLine("Enter the first number:");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        int num2 = int.Parse(Console.ReadLine());
+        int sum = AddNumbers(num1, num2);
+        Console.WriteLine($"The sum of {num1} and {num2} is: {sum}");
+        Console.ReadLine();
+        static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}

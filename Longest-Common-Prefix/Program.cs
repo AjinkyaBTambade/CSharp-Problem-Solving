d204010a0241871c7713ec6d009737e41a6b335f
Ajinkya <ajinkyatambade143@gmail.com>
2024-03-21 21:26:36 +0530

    Longest Common Prefix

---
+++ Longest-Common-Prefix/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LongestCommonPrefixApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] strings = { "book", "boot", "box",};
+            Console.WriteLine(LongestCommonPrefix(strings));
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length == 0)
+                return "";
+
+            Array.Sort(strs);
+            string first = strs[0];
+            string last = strs[strs.Length - 1];
+
+            int i = 0;
+            while (i < first.Length && i < last.Length && first[i] == last[i])
+            {
+                i++;
+            }
+
+            return first.Substring(0, i);
+        }
+    }
+}

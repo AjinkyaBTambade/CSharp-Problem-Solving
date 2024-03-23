49d4bcf8c7d03a5e9bbdad497bb2f9071db3fa44
Ajinkya <ajinkyatambade143@gmail.com>
2024-03-23 20:21:46 +0530

    Greatest Number

--- Factorial-Of-Number/obj/Debug/net8.0/Factorial-Of-Number.AssemblyInfo.cs
+++ Factorial-Of-Number/obj/Debug/net8.0/Factorial-Of-Number.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Factorial-Of-Number")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13f8bdbd77746fccf48d55abb4c807b5a683bb04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d204010a0241871c7713ec6d009737e41a6b335f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Factorial-Of-Number")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Factorial-Of-Number")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Greatest-Number/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter your number:");
+int num1=int.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Enter your number:");
+int num2=int.Parse(Console.ReadLine());
+
+if(num1==num2){
+  Console.WriteLine("Both are equal numbers");
+}
+
+else if(num1>num2)
+{
+  Console.WriteLine($" Biggest number is :{num1}");  
+}
+
+else{
+    Console.WriteLine($" Biggest number is :{num2}"); 
+}
+

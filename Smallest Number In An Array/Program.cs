fbac6dcb99a9e199a316251a5e24dbe023ebe198
Ajinkya <ajinkyatambade143@gmail.com>
2024-04-21 13:26:26 +0530

    Smallest NO. In Array

---
+++ Smallest Number In An Array/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         int[] numbers = { 5, 3, 9, 1, 7 };
+
+//         int smallest = numbers.Min();
+
+//         Console.WriteLine("The smallest element in the array is: " + smallest);
+//     }
+// }
+
+
+// using System;
+
+
+// By USing Loop
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 5, 3, 9, 1, 7, 0 };
+
+        int smallest = numbers[0]; // Assume the first element is the smallest
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] < smallest)
+            {
+                smallest = numbers[i];
+            }
+        }
+
+        Console.WriteLine("The smallest element in the array is: " + smallest);
+    }
+}
